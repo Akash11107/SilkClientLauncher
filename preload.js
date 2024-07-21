c3701aa5a8329e2c08083ffb329ef008083f802f
Akash11107 <155412954+Akash11107@users.noreply.github.com>
2024-07-21 13:40:09 +0530

    v1.0.0

---
+++ preload.js
@@ -0,0 +1,31 @@
+/**
+ * The preload script runs before `index.html` is loaded
+ * in the renderer. It has access to web APIs as well as
+ * Electron's renderer process modules and some polyfilled
+ * Node.js functions.
+ *
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+const { contextBridge, ipcRenderer, shell } = require("electron");
+
+contextBridge.exposeInMainWorld(
+  "IPC", {
+  send: (channel, data) => {
+    ipcRenderer.send(channel, data);
+  },
+  sendSync: (channel, data) => {
+    return ipcRenderer.sendSync(channel, data);
+  },
+  on: (channel, func) => {
+    ipcRenderer.on(channel, (event, ...args) => func(...args));
+  },
+  invoke: (channel, data) => {
+    return ipcRenderer.invoke(channel, data);
+  }
+});
+
+contextBridge.exposeInMainWorld("shell", {
+  openExternal: (url) => {
+    shell.openExternal(url);
+  }
+});
